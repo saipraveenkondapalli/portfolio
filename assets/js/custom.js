76abf3039800a2b0a65925d305186c5b19820894
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2023-01-29 16:28:38 +0530

    added contact form

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,30 @@
-function openInNewTab(url) {
-  window.open(url, "_blank");
-}
 
-$(document).ready(function() {
-  $("#myList li").click(function() {
-    $("#myModal").show();
-  });
 
-  $(".close").click(function() {
-    $("#myModal").hide();
-  });
-});
+
+GitHubCalendar(".calendar", "saipraveenkondapalli");
+        // or enable responsive functionality
+        GitHubCalendar(".calendar", "saipraveenkondapalli", { responsive: true });
+
+GitHubActivity.feed({
+        username: "saipraveenkondapalli",
+        //repository: "your-repo", // optional
+        selector: "#feed",
+        limit: 5, // optional
+      });
+
+
+
+var targetNode = document.body;
+    var config = { attributes: true, childList: true, subtree: true };
+    var observer = new MutationObserver(function(mutationsList) {
+      for(var mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          var img = document.querySelector("img[src='https://avatars.githubusercontent.com/u/63105626?v=4']");
+          if(img) {
+            img.style.display = "none";
+            observer.disconnect();
+          }
+        }
+      }
+    });
+    observer.observe(targetNode, config);
