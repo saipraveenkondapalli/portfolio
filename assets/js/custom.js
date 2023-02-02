c0ce583a521f3cb2b8b97c7a09e1936d1e154d51
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2023-02-02 13:13:05 +0530

    removed github cal, fixed git feed, fixed forms issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,29 +3,17 @@ GitHubCalendar(".calendar", "saipraveenkondapalli");
         // or enable responsive functionality
         GitHubCalendar(".calendar", "saipraveenkondapalli", { responsive: true });
 
+
 GitHubActivity.feed({
-        username: "saipraveenkondapalli",
-        //repository: "your-repo", // optional
-        selector: "#feed",
-        limit: 5, // optional
-      });
+            username: "saipraveenkondapalli",
+            //repository: "your-repo", // optional
+            selector: "#feed",
+            limit:5, // optional
+        });
+
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-// ---------------------------------- cookie check to the success message ----------------------------------------------
-  if (localStorage.getItem("formSuccess") === "true") {
-    document.getElementById("success-alert").style.display = "block";
-    localStorage.removeItem("formSuccess");
-    setTimeout(function() {
-      var alert = document.getElementById("success-alert");
-      alert.classList.remove("show");
-      alert.classList.add("fade");
-      setTimeout(function() {
-        alert.style.display = "none";
-        localStorage.removeItem("formSuccess"); // remove after closing the message
-      }, 500);
-    }, 3000);
-  }
 //----------------------------------------------------------------------------------------------------------------------
 
 // -----------------------------------------------------Remove Profile Pic----------------------------------------------
