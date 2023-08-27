80aee0c7609585d81d63998a898d5bb3eb8d9d78
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2023-08-27 15:24:42 +0530

    added tracker feature

---
+++ assets/js/resume.js
@@ -0,0 +1,22 @@
+// fetch request to flask-apis.vercel.app/api/tracker?id=
+// get id from URL
+
+function resume_tracker(id) {
+    // using fetch API
+    fetch("https://flask-apis.vercel.app/api/tracker?id=" + id)
+        .then(response => response.json())
+        .then(data => {
+                console.log(data);
+            }
+        )
+        .catch(error => console.log(error));
+
+}
+
+
+// get id from URL param 'id'
+var urlParams = new URLSearchParams(window.location.search);
+var id = urlParams.get('id');
+// remove  first character from id
+id = id.substring(1);
+resume_tracker(id);
